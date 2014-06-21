4e9ede617b3b42d17e371ac73268141fe35ebc91
Marcia Van Bel <marcia.van.bel@gmail.com>
2014-06-21 23:56:05 +0200

    Billboarding, camera code & grass texture
    Added basic billboarding (Billboarding.cs)
    Added code to rotate camera & changed it a bit (CameraMovement.cs)
    Added grass texture and put it on terrain
    Attached light to camera for now, possibly we might do something else
    for this

---
+++ Code/Survival Game/Assets/Scripts/Billboard.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour {
+
+	private Camera _camera;
+
+	void Start ()
+	{
+		//Find camera and set it
+		_camera = Camera.main;
+
+		//If no camera is found, generate warning
+		if (_camera == null)
+			Debug.Log("Camera variable is null!");
+
+	}
+
+	void Update ()
+	{
+		Vector3 target = new Vector3(_camera.transform.position.x,this.transform.position.y,_camera.transform.position.z);
+
+		transform.LookAt(target);
+		transform.Rotate(Vector3.up,180);
+	}
+}
--- Code/Survival Game/Assets/Scripts/CameraMovement.cs
+++ Code/Survival Game/Assets/Scripts/CameraMovement.cs
@@ -7,13 +7,27 @@ public class CameraMovement : MonoBehaviour {
 
 	private Vector3 _cameraOffset;
 
-	// Use this for initialization
 	void Start () {
 		_cameraOffset = _hero.transform.position -  this.transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void LateUpdate () {
+
+		//Check if the camera needs to be turned
+		if (Input.GetButtonUp("TurnCameraCW"))
+		{
+			this.transform.RotateAround(_hero.transform.position,Vector3.up,90);
+			_cameraOffset = _hero.transform.position -  this.transform.position;
+		}
+		
+		else if (Input.GetButtonUp("TurnCameraCCW"))
+		{
+			this.transform.RotateAround(_hero.transform.position,Vector3.up,-90);
+			_cameraOffset = _hero.transform.position -  this.transform.position;
+		}
+
+		//Set the camera's pos & target
 		this.transform.position = _hero.transform.position - _cameraOffset;
+		this.transform.LookAt(_hero.transform);
 	}
 }
